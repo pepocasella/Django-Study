f6ac82b6203cc6e03936f056e1ae1df494a48269
Pedro_Casella <pepocasella@gmail.com>
2024-03-24 18:13:29 -0300

    add admin features

--- CRM-Example/leads/models.py
+++ CRM-Example/leads/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
 
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
