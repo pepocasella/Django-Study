5ca44a17ac141df1e9c08799f449ebab377303bd
Pedro_Casella <pedrocasellarodrigues@gmail.com>
2024-03-25 09:18:52 -0300

    adding urls logics
    Every single app need to own its urls routes

--- CRM-Example/leads/views.py
+++ CRM-Example/leads/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse("Hello World")
+    context = {
+        'name':'Joe',
+        'age':35
+    }
+    return render(request, 'second_page.html', context)
